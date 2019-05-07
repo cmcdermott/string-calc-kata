268bd9380228436963dbd8b5927eebf082288aa0
Chris McDermott <chrism@pageuppeople.com>
2019-05-07 10:49:33 +1000

    Step 1 - string calculator

---
+++ StringCalc/StringCalc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalc
+{
+    public class StringCalc
+    {
+        public int Add(string numbers) // "1,2,3,4,5"
+        {
+            int[] numbersArray = numbers.Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+            int currentNumber;
+            int sum =0;
+
+            for(int i = 0; i < numbersArray.Length -1; i++)
+            {
+                currentNumber = numbersArray[i];
+                sum = sum + currentNumber;
+
+                
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ StringCalc/TestCalc.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace StringCalc
+{
+    public class TestCalc
+    {
+
+        [Fact]
+        public void Test()
+        {
+            var test1 = "1,1,2";
+            var testClass = new StringCalc();
+
+            var output = testClass.Add(test1);
+
+            Assert.Equal(4, output);
+        }
+    }
+}
