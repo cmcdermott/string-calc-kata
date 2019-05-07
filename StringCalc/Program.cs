6a92f289a2e6b30be4a4230d512f184db054dfe1
Chris McDermott <chrism@pageuppeople.com>
2019-05-07 14:13:09 +1000

    Step 6

---
+++ StringCalc/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class StringCalc
+    {
+        public int Add(string numbers) // "1,2,3,4,5"
+        {
+            var sum = 0;
+            var delimter = new List<string> { ",", "\n" };
+
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+            
+            numbers = FindDelimiter(numbers, delimter);
+            var numbersArray = SplitNumberString(numbers, delimter);
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                CheckNegative(numbersArray[i]);
+
+                if (numbersArray[i] <= 1000)
+                {
+                    var currentNumber = numbersArray[i];
+                    sum += currentNumber;
+                }
+            }
+
+            return sum;
+        }
+
+        private static int[] SplitNumberString(string numbers, List<string> delimter)
+        {
+            return numbers.Split(delimter.ToArray(), new StringSplitOptions()).Select(x => Convert.ToInt32(x)).ToArray();
+        }
+
+        private static string FindDelimiter(string numbers, List<string> delimter)
+        {
+            if (!numbers.StartsWith("//"))
+                return numbers;
+            
+            var newDelimiter = numbers.Substring(2, 1);
+            delimter.Add(newDelimiter);
+
+            return numbers.Remove(0, 4);
+        }
+
+        private static void CheckNegative(int i)
+        {
+            if (i < 0)
+                throw new Exception("Negatives not allowed: " + i);
+        }
+    }
+}
--- StringCalc/TestCalc.cs
+++ StringCalc/TestCalc.cs
@@ -1,12 +1,13 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
-namespace StringCalc
+namespace ConsoleApp1
 {
     public class TestCalc
     {
 
         [Fact]
-        public void Test()
+        public void step1()
         {
             var test1 = "1,1,2";
             var testClass = new StringCalc();
@@ -14,6 +15,53 @@ namespace StringCalc
             var output = testClass.Add(test1);
 
             Assert.Equal(4, output);
+        }
+
+        [Fact]
+        public void step3()
+        {
+            var test3 = "1\n2\n3";
+            var testClass3 = new StringCalc();
+
+            var output = testClass3.Add(test3);
+
+            Assert.Equal(6, output);
+        }
+
+        [Fact]
+        public void step4()
+        {
+            var test4 = "//;\n1;2";
+            var testClass4 = new StringCalc();
+
+            var output = testClass4.Add(test4);
+
+            Assert.Equal(3, output);
+        }
+
+        [Fact]
+        public void step5()
+        {
+            var test5 = "1,4,-1";
+            var testClass5 = new StringCalc();
+
+            var ex = Assert.Throws<Exception>(() => { testClass5.Add(test5); });
+
+            Assert.Equal("Negatives not allowed: -1", ex.Message);
+        }
+
+        [Fact]
+        public void step6()
+        {
+            var test6 = "2,1001";
+            var testClass6 = new StringCalc();
+
+            var output = testClass6.Add(test6);
+
+            Assert.Equal(2, output);
+
+
+           
         }
     }
 }
